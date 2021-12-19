dc79b90f85aeb76109452a6969911240ae476dc4
ChingTseTing <81843679+ChingTseTing@users.noreply.github.com>
2021-12-19 12:00:50 +0800

    Update fintech_final_project.py

--- fintech_final_project.py
+++ fintech_final_project.py
@@ -113,8 +113,7 @@ def handle_message(event):
                             URIAction(
                                 label='馬上查看',
                                 uri='https://marketingliveincode.com/?page_id=270'
-                            ),
-                            PostbackAction(label='ping with text', data='ping1', text='ping2')
+                            )
                         ]
                     ),
                     CarouselColumn(
